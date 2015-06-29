a78e805d9b7d8dd71056659933948c07bf224826
kamisama <1457434343@qq.com>
2015-06-29 12:49:38 +0800

    add icon and update fireball-packages/console/#22

--- panel/item.js
+++ panel/item.js
@@ -21,10 +21,28 @@ Polymer({
             type: Boolean,
             value: false,
         },
+
+        folded: {
+            type: Boolean,
+            value: false,
+            observer: '_foldInChanged',
+            reflectToAttribute: true,
+        },
     },
 
     _typeClass: function ( type ) {
         return 'item layout horizontal ' + type;
+    },
+
+    _iconClass: function (type) {
+        switch (type) {
+            case 'error':
+                return 'fa fa-times-circle icon';
+            break;
+            case 'warn':
+                return 'fa fa-warning icon';
+            break;
+        }
     },
 
     _showCount: function ( showCount, count ) {
@@ -37,5 +55,18 @@ Polymer({
 
     _computedCount: function ( count ) {
         return count + 1;
+    },
+
+    _onClickFold: function () {
+        this.folded = !this.folded;
+    },
+
+    _foldInChanged: function () {
+        if (this.folded) {
+            this.foldIconClass = 'fa fold fa-caret-down';
+        }
+        else {
+            this.foldIconClass = 'fa fold fa-caret-right';
+        }
     },
 });
