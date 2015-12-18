e0fa81d08056deae8bb2d904b2fd9ecd1798f45f
Johnny Wu <KarasAya@gmail.com>
2015-12-19 00:42:57 +0800

    send _console:clear instead

--- panel/console.js
+++ panel/console.js
@@ -1,181 +1,188 @@
-(function () {
-Editor.registerPanel( 'console.panel', {
+(() => {
+  'use strict';
+
+  Editor.registerPanel( 'console.panel', {
     properties: {
-        logs: {
-            type: Array,
-            value: function () {
-                return [];
-            },
+      logs: {
+        type: Array,
+        value () {
+          return [];
         },
+      },
 
-        filterOption: {
-            type: String,
-            value: 'All',
-        },
+      filterOption: {
+        type: String,
+        value: 'All',
+      },
 
-        filterText: {
-            type: String,
-            value: '',
-        },
+      filterText: {
+        type: String,
+        value: '',
+      },
 
-        useRegex: {
-            type: Boolean,
-            value: false,
-        },
+      useRegex: {
+        type: Boolean,
+        value: false,
+      },
 
-        collapse: {
-            type: Boolean,
-            value: true,
-        },
+      collapse: {
+        type: Boolean,
+        value: true,
+      },
 
-        logsCount: {
-            type: Number,
-            value: 0,
-        },
+      logsCount: {
+        type: Number,
+        value: 0,
+      },
     },
 
-    ready: function () {
-        Editor.sendRequestToCore( 'console:query', function ( results ) {
-            for ( var i = 0; i < results.length; ++i ) {
-                var item = results[i];
-                this.add( item.type, item.message );
-            }
-        }.bind(this));
+    ready () {
+      Editor.sendRequestToCore( 'console:query', results => {
+        for ( let i = 0; i < results.length; ++i ) {
+          let item = results[i];
+          this.add( item.type, item.message );
+        }
+      });
     },
 
-    'console:log': function ( message ) {
-        this.add( 'log', message );
+    'console:log' ( message ) {
+      this.add( 'log', message );
     },
 
-    'console:success': function ( message ) {
-        this.add( 'success', message );
+    'console:success' ( message ) {
+      this.add( 'success', message );
     },
 
-    'console:failed': function ( message ) {
-        this.add( 'failed', message );
+    'console:failed' ( message ) {
+      this.add( 'failed', message );
     },
 
-    'console:info': function ( message ) {
-        this.add( 'info', message );
+    'console:info' ( message ) {
+      this.add( 'info', message );
     },
 
-    'console:warn': function ( message ) {
-        this.add( 'warn', message );
+    'console:warn' ( message ) {
+      this.add( 'warn', message );
     },
 
-    'console:error': function ( message ) {
-        this.add( 'error', message );
+    'console:error' ( message ) {
+      this.add( 'error', message );
     },
 
-    'console:clear': function () {
-        this.clear();
+    'console:clear' () {
+      this._clear();
     },
 
-    add: function ( type, text ) {
-        var desc = text.split('\n')[0];
-        var detail = '';
-        var firstLine = text.indexOf('\n');
-        if (firstLine > 0) {
-            detail = text.substring(firstLine + 1);
+    add ( type, text ) {
+      let desc = text.split('\n')[0];
+      let detail = '';
+      let firstLine = text.indexOf('\n');
+
+      if (firstLine > 0) {
+        detail = text.substring(firstLine + 1);
+      }
+
+      this.push('logs', {
+        type: type,
+        text: text,
+        desc: desc,
+        detail: detail,
+        count: 0,
+      });
+      this.logsCount = this.logs.length;
+
+      // to make sure after layout and before render
+      if ( !this._scrollTaskID ) {
+        this._scrollTaskID = window.requestAnimationFrame (() => {
+          this._scrollTaskID = null;
+          this.$.view.scrollTop = this.$.view.scrollHeight;
+        } );
+      }
+    },
+
+    clear () {
+      this._clear();
+      Editor.sendToCore('_console:clear');
+    },
+
+    _clear () {
+      this.logs = [];
+      this.logsCount = this.logs.length;
+    },
+
+    applyFilter ( logsCount, filterText, filterOption, useRegex, collapse ) {
+      var filterLogs = [];
+      var type = filterOption.toLowerCase();
+
+      var filter;
+      if ( useRegex ) {
+        try {
+          filter = new RegExp(filterText);
+        }
+        catch ( err ) {
+          filter = new RegExp('');
+        }
+      }
+      else {
+        filter = filterText.toLowerCase();
+      }
+
+      var i = 0;
+      var log = null;
+
+      for ( i = 0; i < this.logs.length; ++i ) {
+        var log_ = this.logs[i];
+
+        log = {
+          type: log_.type,
+          text: log_.text,
+          desc: log_.desc,
+          detail: log_.detail,
+          count: 0,
+        };
+
+        if ( type !== 'all' && log.type !== type ) {
+          continue;
         }
 
-        this.push('logs', {
-            type: type,
-            text: text,
-            desc: desc,
-            detail: detail,
-            count: 0,
-        });
-        this.logsCount = this.logs.length;
-
-        // to make sure after layout and before render
-        if ( !this._scrollTaskID ) {
-            this._scrollTaskID = window.requestAnimationFrame ( function () {
-                this._scrollTaskID = null;
-                this.$.view.scrollTop = this.$.view.scrollHeight;
-            }.bind(this) );
-        }
-    },
-
-    clear: function () {
-        this.logs = [];
-        this.logsCount = this.logs.length;
-        Editor.sendToCore('console:clear');
-    },
-
-    applyFilter: function ( logsCount, filterText, filterOption, useRegex, collapse ) {
-        var filterLogs = [];
-        var type = filterOption.toLowerCase();
-
-        var filter;
         if ( useRegex ) {
-            try {
-                filter = new RegExp(filterText);
-            }
-            catch ( err ) {
-                filter = new RegExp('');
-            }
+          if ( !filter.exec(log.text) ) {
+            continue;
+          }
         }
         else {
-            filter = filterText.toLowerCase();
+          if ( log.text.toLowerCase().indexOf(filter) === -1 ) {
+            continue;
+          }
         }
 
-        var i = 0;
-        var log = null;
+        filterLogs.push(log);
+      }
 
-        for ( i = 0; i < this.logs.length; ++i ) {
-            var log_ = this.logs[i];
 
-            log = {
-                type: log_.type,
-                text: log_.text,
-                desc: log_.desc,
-                detail: log_.detail,
-                count: 0,
-            };
+      if ( collapse && filterLogs.length > 0 ) {
+        var collapseLogs = [];
+        var lastLog = filterLogs[0];
 
-            if ( type !== 'all' && log.type !== type ) {
-                continue;
-            }
+        collapseLogs.push( lastLog );
 
-            if ( useRegex ) {
-                if ( !filter.exec(log.text) ) {
-                    continue;
-                }
-            }
-            else {
-                if ( log.text.toLowerCase().indexOf(filter) === -1 ) {
-                    continue;
-                }
-            }
+        for ( i = 1; i < filterLogs.length; ++i ) {
+          log = filterLogs[i];
 
-            filterLogs.push(log);
+          if ( lastLog.text === log.text && lastLog.type === log.type ) {
+            lastLog.count += 1;
+          }
+          else {
+            collapseLogs.push( log );
+            lastLog = log;
+          }
         }
 
+        filterLogs = collapseLogs;
+      }
 
-        if ( collapse && filterLogs.length > 0 ) {
-            var collapseLogs = [];
-            var lastLog = filterLogs[0];
-
-            collapseLogs.push( lastLog );
-
-            for ( i = 1; i < filterLogs.length; ++i ) {
-                log = filterLogs[i];
-
-                if ( lastLog.text === log.text && lastLog.type === log.type ) {
-                    lastLog.count += 1;
-                }
-                else {
-                    collapseLogs.push( log );
-                    lastLog = log;
-                }
-            }
-
-            filterLogs = collapseLogs;
-        }
-
-        return filterLogs;
+      return filterLogs;
     },
-});
+  });
 
 })();
